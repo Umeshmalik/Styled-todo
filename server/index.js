86dfdae03d7d2f2ee97c7c2d0557281ac074e72d
Umesh-malik-byjus <umesh.malik@byjus.com>
2022-10-12 00:46:54 +0530

    added bp

--- server/index.js
+++ server/index.js
@@ -1,18 +1,20 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require("cors");
+const bodyParser = require('body-parser');
 const { connect } = require('mongoose');
-const routes = require('./routes')
 
 const app = express();
 
-app.use(express.json());
-app.use(cors())
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json({limit: '10mb'}))
+app.use(cors());
 
 connect(process.env.MONGO_DB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
+    const routes = require('./routes')
     app.use(routes())
     app.listen(process.env.PORT, () => {
         console.log(`server started at ${process.env.PORT}`)
